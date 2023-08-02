9d7cc3890b5d4a4a65cce14ea7b58e0ad0e7f9dc
rudysant <rudysant@gmail.com>
2023-08-02 07:02:04 +0700

    First commit

--- app_unioncat/views.py
+++ app_unioncat/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+import requests
 
 def home(request):
-    return render(request, 'home.html')
+    response=requests.get('http://localhost:8000/api/books').json()
+    return render(request, 'home.html', {'response':response})
+
+def detail(request, id):
+    response=requests.get('http://localhost:8000/api/books/{{id}}').json()
+    return render(request, 'detail.html', {'response':response})
 
 # Create your views here.
